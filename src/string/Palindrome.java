4f0df8b0d7452337cba5c09a0a15dccad40b5daa
arun-sridhar-001 <sridhararun947@gmail.com>
2025-10-25 19:59:47 +0530

    "Palindrome and Bitwise"

--- src/string/Palindrome.java
+++ src/string/Palindrome.java
@@ -1,4 +1,42 @@
 package string;
 
 public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String a = "Amma";
+
+
+        boolean isPalindrome = checkPalindrome(a);
+
+        // Enhanced if loop
+        String message = isPalindrome ? "This is palindrome" : "This is Not a palindrome";
+
+//        if(isPalindrome && true) {
+//            message = "This is palindrome";
+//
+//        } else {
+//            message = "This is Not a palindrome";
+//        }
+        System.out.println(message);
+
+    }
+
+    private static boolean checkPalindrome(String a) {
+        a = a.toLowerCase();
+        a.replace('A', 'S');
+        int left = 0;
+        int right = a.length()-1;
+
+        while(left < right) {
+            if(a.charAt(left) != a.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
 }
--- src/string/StringProgram.java
+++ src/string/StringProgram.java
@@ -1,4 +1,40 @@
 package string;
 
+import java.util.Objects;
+
 public class StringProgram {
+
+    public static void main(String[] args) {
+
+        String a = "exampleA";
+        System.out.println(System.identityHashCode(a));  // 1705736037
+        String b = "exampleA";
+        System.out.println(System.identityHashCode(b)); // 1705736037
+
+        b = "exampleB";
+        System.out.println(System.identityHashCode(b));
+
+
+        String objectType = new String("exampleA");
+        System.out.println(System.identityHashCode(objectType));
+
+
+        String inputFromGFG = new String("cricket");
+
+        String dataBase = "cricket";
+        String s2 = inputFromGFG;
+
+        System.out.println(dataBase == s2);
+        System.out.println(dataBase.equals(s2));
+
+        StringBuffer buffer = new StringBuffer();  // threadSafe
+
+        for(int i = 0; i< 10; i++) {
+            buffer = buffer.append(dataBase+i);
+            System.out.println(buffer);
+            System.out.println(System.identityHashCode(buffer));
+        }
+
+    }
+
 }
