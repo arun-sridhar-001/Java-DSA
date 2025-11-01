24692b8fbf450e941b6e571479b8f8d8f78f74b6
arun-sridhar-001 <sridhararun947@gmail.com>
2025-11-01 20:08:23 +0530

    Bubble Sort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,33 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] num = {6, 5, 2, 8, 9, 4};  // 2, 4,5,6,8,9
+
+        int temp = 0;
+        long startTime = System.currentTimeMillis();
+        for(int i=0; i<num.length; i++) {
+            for(int j=0; j<num.length-i-1; j++) {
+                if(num[j] > num[j+1]) {
+                    temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                }
+            }
+        }
+
+
+        Arrays.sort(num);
+
+        System.out.println(System.currentTimeMillis() - startTime+ " ms");
+
+        for(int i: num) {
+            System.out.println(i);
+        }
+
+    }
+}
