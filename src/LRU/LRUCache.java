486eb588fc57a946c3a3f4a166cac7b35c199df3
arun-sridhar-001 <sridhararun947@gmail.com>
2026-04-23 20:45:08 +0530

    changes: newNode has added in head.next

--- src/LRU/LRUCache.java
+++ src/LRU/LRUCache.java
@@ -10,7 +10,8 @@ import java.util.Map;
  *
  * LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
  * int get(int key) Return the value of the key if the key exists, otherwise return -1.
- * void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+ * void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+ * If the number of keys exceeds the capacity from this operation, evict the least recently used key.
  * The functions get and put must each run in O(1) average time complexity.
  *
  *
@@ -83,7 +84,7 @@ public class LRUCache {
         newNode.next = nextNode;
         newNode.prev = head;
 
-        head.next = nextNode;
+        head.next = newNode;
         nextNode.prev = newNode;
     }
 
